edd5c664a2aa3a5d073385d12a192cb6b2119d67
samueltoepke <samueltoepke@gmail.com>
2018-04-19 13:22:55 -0400

    Space management.

--- src/main/java/samueltoepke/TimeController.java
+++ src/main/java/samueltoepke/TimeController.java
@@ -25,75 +25,75 @@ import com.google.gson.JsonObject;
 
 @RestController
 public class TimeController {
-	
+
     @RequestMapping("/gettime")
     public String getTips(@RequestParam(value="tz", defaultValue="GMT") String tz) throws IOException, ParseException {
         String proc = "  getTime: ";
-		String togo = "";
-		///System.out.println(proc + "STARTING.");
+        String togo = "";
+        ///System.out.println(proc + "STARTING.");
 
-		///////////////////
-		// 0. Error Checking
-		
-		// Check that tz is populated.
-		if (StringUtils.isEmpty(tz) == true) 
-			return populateErrorMessage("ERROR. tz for getTime() is not populated. Pass 'GMT' or proper tz from list: https://garygregory.wordpress.com/2013/06/18/what-are-the-java-timezone-ids/");
-		
-		// Check that tz is a legal value.
-		boolean legalTZ = false;
-		String[] ids = TimeZone.getAvailableIDs();
-		for (String id : ids) {
-			if (TimeZone.getTimeZone(id).getID().equals(tz)) {
-				legalTZ = true;
-				break;
-			}
-		}
-		
-		if (legalTZ == false)
-			return populateErrorMessage("ERROR. tz for getTime() is of an incorrect format. Pass 'GMT' or proper tz from list: https://garygregory.wordpress.com/2013/06/18/what-are-the-java-timezone-ids/");
-		
-		///////////////////
-		// 1. Get Time String		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
-		sdf.setTimeZone(TimeZone.getTimeZone(tz));
+        ///////////////////
+        // 0. Error Checking
+        
+        // Check that tz is populated.
+        if (StringUtils.isEmpty(tz) == true) 
+            return populateErrorMessage("ERROR. tz for getTime() is not populated. Pass 'GMT' or proper tz from list: https://garygregory.wordpress.com/2013/06/18/what-are-the-java-timezone-ids/");
 
-		Date now = new Date();
-		String strDate = sdf.format(now);
-		
-		// Add time fields to JSON object to return.
-		JsonObject jo = new JsonObject();
-		jo.addProperty("time_string", strDate);
-		jo.addProperty("tz", tz);
-		togo = jo.toString();
+        // Check that tz is a legal value.
+        boolean legalTZ = false;
+        String[] ids = TimeZone.getAvailableIDs();
+        for (String id : ids) {
+            if (TimeZone.getTimeZone(id).getID().equals(tz)) {
+                legalTZ = true;
+                break;
+            }
+        }
 
-		///System.out.println(proc + "ENDING.");
-		return togo;
+        if (legalTZ == false)
+            return populateErrorMessage("ERROR. tz for getTime() is of an incorrect format. Pass 'GMT' or proper tz from list: https://garygregory.wordpress.com/2013/06/18/what-are-the-java-timezone-ids/");
+
+        ///////////////////
+        // 1. Get Time String		
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone(tz));
+
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        
+        // Add time fields to JSON object to return.
+        JsonObject jo = new JsonObject();
+        jo.addProperty("time_string", strDate);
+        jo.addProperty("tz", tz);
+        togo = jo.toString();
+
+        ///System.out.println(proc + "ENDING.");
+        return togo;
     }
 
-	/*
-	 * populateErrorMessage(). Takes a non-null String, and creates/returns
-	 *   a JSON error message.
-	 *   
-	 * @author Samuel Lee Toepke
-	 * @param Error message
-	 * @return String representation in JSON.
-	 * 
-	 * @version 1.0
-	 */
-	private static String populateErrorMessage(String message) {
-		String togo = "";
-		
-		if (StringUtils.isEmpty(message) ) { 
-			System.err.println("ERROR. populateErrorMessage() contains an improper input value.");
-			System.exit(1);
-		}
-			    
-		JsonObject jo = new JsonObject();
-		jo.addProperty("status", "400");
-		jo.addProperty("error", "Bad Request");
-		jo.addProperty("message", message);
-		togo = jo.toString();
-	
-		return togo;
-	}
+    /*
+     * populateErrorMessage(). Takes a non-null String, and creates/returns
+     *   a JSON error message.
+     *   
+     * @author Samuel Lee Toepke
+     * @param Error message
+     * @return String representation in JSON.
+     * 
+     * @version 1.0
+     */
+    private static String populateErrorMessage(String message) {
+        String togo = "";
+        
+        if (StringUtils.isEmpty(message) ) { 
+            System.err.println("ERROR. populateErrorMessage() contains an improper input value.");
+            System.exit(1);
+        }
+
+        JsonObject jo = new JsonObject();
+        jo.addProperty("status", "400");
+        jo.addProperty("error", "Bad Request");
+        jo.addProperty("message", message);
+        togo = jo.toString();
+
+        return togo;
+    }
 }
